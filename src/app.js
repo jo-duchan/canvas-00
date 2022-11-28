71ab711016e670e52c70c2255af0fe3282a6ecc7
jo-duchan <jo_duchan@icloud.com>
2022-11-28 23:45:03 +0900

    DrawInage, handleResize

--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 const canvas = document.querySelector("#canvasElement");
 const ctx = canvas.getContext("2d");
+const image = new Image();
 
-// canvas init
-canvas.width = 1920;
-canvas.height = 1080;
+const handleResize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const imgRatio = image.width / image.height;
+  const winRatio = window.innerWidth / window.innerHeight;
+  let renderW, renderH, xStart, yStart;
 
-canvas.addEventListener("click", () => {
-  console.log("canvas", canvas);
-});
+  if (imgRatio < winRatio) {
+    renderW = image.width * (window.innerHeight / image.height);
+    renderH = window.innerHeight;
+    xStart = (window.innerWidth - renderW) / 2;
+    yStart = 0;
+  } else if (imgRatio > winRatio) {
+    renderW = window.innerWidth;
+    renderH = image.height * (window.innerWidth / image.width);
+    xStart = 0;
+    yStart = (window.innerHeight - renderH) / 2;
+  } else {
+    renderW = window.innerWidth;
+    renderH = window.innerHeight;
+    xStart = 0;
+    yStart = 0;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(image, xStart, yStart, renderW, renderH);
+
+  // ctx.globalCompositeOperation = "overlay";
+  // ctx.fillStyle = "#FFFFFF60";
+  // ctx.fillRect(xStart, yStart, renderW, renderH);
+};
+
+const handleCanvasLoad = () => {
+  image.src = "./assets/europeana.jpg";
+  image.onload = () => {
+    handleResize();
+  };
+};
+
+window.addEventListener("DOMContentLoaded", handleCanvasLoad);
+window.addEventListener("resize", handleResize);
