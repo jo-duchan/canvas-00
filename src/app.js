f251f476230dffb9d2d560e54e751ae079aad089
jo-duchan <jo_duchan@icloud.com>
2022-12-06 01:58:00 +0900

    canvas image size 작업

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ image.src = "./assets/europeana.jpg";
 let isLoaded = false;
 let stageWidth = 0;
 let stageHeight = 0;
+const pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 const imgPos = {
   x: 0,
   y: 0,
@@ -13,14 +14,41 @@ const imgPos = {
 };
 
 const handleResize = () => {
-  // drawImage 초기화
+  stageWidth = document.body.clientWidth;
+  stageHeight = document.body.clientHeight;
+
+  canvas.width = stageWidth * pixelRatio;
+  canvas.height = stageHeight * pixelRatio;
+  ctx.scale(pixelRatio, pixelRatio);
+
   if (isLoaded) {
     handleDraw();
   }
+  console.log(
+    `stageWidth: ${stageWidth}, stageHeight: ${stageHeight},  imgPos.width: ${imgPos.width}, imgPos.height: ${imgPos.height} `
+  );
 };
 
 const handleDraw = () => {
-  // ctx.drawImage();
+  const stageRatio = stageWidth / stageHeight;
+  const imgRatio = image.width / image.height;
+
+  imgPos.width = stageWidth;
+  imgPos.height = stageHeight;
+
+  if (imgRatio > stageRatio) {
+    imgPos.width = Math.round(image.width * (stageHeight / image.height));
+    imgPos.x = Math.round((stageWidth - imgPos.width) / 2);
+  } else {
+    imgPos.height = Math.round(image.height * (stageWidth / image.width));
+    imgPos.y = Math.round((stageHeight - imgPos.height) / 2);
+  }
+
+  ctx.clearRect(0, 0, imgPos.width, imgPos.height);
+  ctx.drawImage(image, imgPos.x, imgPos.y, imgPos.width, imgPos.height);
+  console.log(
+    `imgPos.width: ${imgPos.width}, imgPos.height: ${imgPos.height} `
+  );
 };
 
 const handleLoad = () => {
